96239d6bc791d6ea90f9b729e22abfad6c26a1b8
--global <--global>
2019-03-15 15:09:17 +0200

    Second commit with large edits.

--- firstApp/resources/js/app.js
+++ firstApp/resources/js/app.js
@@ -8,7 +8,23 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import { Form, HasError, AlertError } from 'vform'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+window.Form = Form
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
 
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default },
+    { path: '/users', component: require('./components/Users.vue').default },
+  ]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes // short for `routes: routes`
+})
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -29,5 +45,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
-});
+    router
+  }).$mount('#app')
